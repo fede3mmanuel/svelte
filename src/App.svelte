8bb0c59fe550f7035ccb11c774d16b305a2b5884
Emmanuel Sanchez <fede@fede.website>
2021-08-14 16:45:46 -0500

    Input Types

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,43 @@
 <script>
-	import Card from "./Card.svelte";
+import { text } from "svelte/internal";
+
+	let name = "";
+	let num;
+	let ischecked = true;
+	let value = 2;
+	let selected;
+
+	let questions = [
+		{id: 1, text: "What is your favorite color?"},
+		{id:2, text: "What is your favorite fruit?"}
+	]
 </script>
 
 <main>
-	<Card>
-		
-		<h3>Card</h3>
-		<p>This is our card</p>
-		
-		<button slot="button">Test button</button>
+	<input type="text" bind:value={name}>
+	<input type="number" bind:value={num}>
+	<input type="checkbox" bind:checked={ischecked}>
 
-		<div>
-			<h4>This is below everithing</h4>
-		</div>
-	</Card>
+	<label>
+		<input type="radio" bind:group={value} value={1}>
+		1
+	</label>
+	<label>
+		<input type="radio" bind:group={value} value={2}>
+		2
+	</label>
+	<label>
+		<input type="radio" bind:group={value} value={3}>
+		3
+	</label>
+
+	<select bind:value={selected}>
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
 </main>
 
 <style>
